be8fdf98493d85e846295e430c95babf97effe1a
terurou <terurou@denkiyagi.jp>
2014-09-08 15:41:18 +0900

    Updates.

--- src/hxgnd/js/jsviews/JsViews.hx
+++ src/hxgnd/js/jsviews/JsViews.hx
@@ -1,9 +1,7 @@
 package hxgnd.js.jsviews;
 
 import js.html.Element;
-#if jsviews_enable_jqhx
-import js.jqhx.JqHtml;
-#end
+import hxgnd.js.JqHtml;
 import haxe.ds.Option;
 
 @:native("$")
@@ -38,39 +36,23 @@ extern class JsViews {
     }
 
     @:overload(function (flag: Bool, to: Element, from: {}, ?context: {}): Void{})
-    #if jsviews_enable_jqhx
     @:overload(function (flag: Bool, to: JqHtml, from: {}, ?context: {}): Void{})
-    #end
     @:overload(function (template: Template, to: String, from: {}, ?context: {}): Void{})
     @:overload(function (template: Template, to: Element, from: {}, ?context: {}): Void{})
-    #if jsviews_enable_jqhx
     @:overload(function (template: Template, to: JqHtml, from: {}, ?context: {}): Void{})
-    #end
     static function link(flag: Bool, to: String, from: {}, ?context: {}): Void;
 
     @:overload(function (flag: Bool, to: Element): Void{})
-    #if jsviews_enable_jqhx
     @:overload(function (flag: Bool, to: JqHtml): Void{})
-    #end
     @:overload(function (template: Template, to: String): Void{})
     @:overload(function (template: Template, to: Element): Void{})
-    #if jsviews_enable_jqhx
     @:overload(function (template: Template, to: JqHtml): Void{})
-    #end
     @:overload(function (): Void{})
     static function unlink(flag: Bool, to: String) : Void;
 
-    @:overload(function (object: JsObject<Dynamic>): ObjectObservable{})
-    @:overload(function (object: {}): ObjectObservable{})
-    static function observable(array: Array<Dynamic>) : ArrayObservable;
-
-    static inline function objectObservable(object: JsObject<Dynamic>): ObjectObservable {
-        return untyped __js__("$").observable(object);
-    }
-
-    static inline function arrayObservable(array: Array<Dynamic>): ArrayObservable {
-        return untyped __js__("$").observable(array);
-    }
+    @:overload(function (object: JsObject<Dynamic>): Observable{})
+    @:overload(function (object: {}): Observable{})
+    static function observable(array: Array<Dynamic>) : Observable;
 
     @:overload(function (object: {}, ?path: String, myHandler: ObservableEvent -> ObservableEventArgs -> Void): Void{})
     static function observe(array: Array<Dynamic>, myHandler: ObservableEvent -> ObservableEventArgs -> Void) : Void;
@@ -89,8 +71,8 @@ typedef TagOptions = {
     ?flow: Bool,
     ?init: TagCtx -> Dynamic -> Void,
     ?onBeforeLink: Void -> Bool,
-    ?onAfterLink: TagCtx -> Dynamic -> Void,
-    ?onUpdate: ObservableEvent -> ObservableEventArgs -> TagCtx -> Void,
+    ?onAfterLink: TagCtx -> LinkCtx -> Void,
+    ?onUpdate: ObservableEvent -> ObservableEventArgs -> TagCtx -> Bool,
     ?onBeforeChange: ObservableEvent -> ObservableEventArgs -> Bool,
     ?onDispose: Void -> Void
 }
@@ -103,50 +85,90 @@ typedef TemplateOptions = {
 }
 
 extern class Template {
+    var htmlTag(default, never): String;
     var markup(default, never): String;
 
     function render(?data: {}, ?helpers: JsObject<Dynamic -> Dynamic>, ?noIteration: Bool): String;
 
     @:overload(function (to: Element, from: JsObject<Dynamic>, ?context: {}): Void{})
-    #if jsviews_enable_jqhx
     @:overload(function (to: JqHtml, from: JsObject<Dynamic>, ?context: {}): Void{})
-    #end
     function link(to: String, from: JsObject<Dynamic>, ?context: {}): Void;
 
     @:overload(function (to: Element): Void{})
-    #if jsviews_enable_jqhx
     @:overload(function (to: JqHtml): Void{})
-    #end
     function unlink(to: String): Void;
 }
 
 extern class TagCtx {
     var args(default, never): Array<Dynamic>;
-    var content(default, never): Template;
+    var content(default, never): TmplObject;
     var ctx(default, never): Dynamic;
     var index(default, never): Int;
     var markup(default, never): String;
-    var params(default, never): String;
+    var params(default, never): String; // TODO 要確認:commit55で Array<String> ?
     var props(default, never): JsObject<Dynamic>;
     function render(?data: {}, ?helpers: JsObject<Dynamic -> Dynamic>, ?noIteration: Bool): String;
     var tag(default, never): TagDef;
-    var tmpl(default, never): Dynamic;
+    var tmpl(default, default): TmplObject;
     var views(default, never): Dynamic;
 }
 
-extern class TagDef {
-    var _(default, never): Dynamic<Dynamic>;
-    function contents(selector: String): #if jsviews_enable_jqhx JqHtml #else ArrayAccess<Element>#end;
-    var ctx(default, never): Dynamic;
-    var linkCtx(default, never): Dynamic;
-    var linkedElem(default, default): Null<Dynamic>;
-    var parentElem(default, never): Element;
-    function refresh(?sourceValue: Dynamic): Void;
-    var tagCtx(default, never): TagCtx;
-    var tagName(default, never): String;
-    function update(value: Dynamic): Void;
+abstract TagDef(Dynamic) {
+    public var _(get, never): Dynamic<Dynamic>;
+    public var ctx(get, never): Dynamic;
+    public var linkCtx(get, never): LinkCtx;
+    public var linkedElem(get, set): Null<Dynamic>;
+    public var parentElem(get, never): Element;
+    public var tagCtx(get, never): TagCtx;
+    public var tagName(get, never): String;
+    public var template(get, set): Null<Template>;
+
+    public inline function contents(selector: String): JqHtml {
+        return this.contents(selector);
+    }
+
+    public inline function refresh(?sourceValue: Dynamic): Void {
+        this.refresh(sourceValue);
+    }
+
+    public inline function update(value: Dynamic): Void {
+        this.update(value);
+    }
+
+    @:arrayAccess inline function get(key: String): Null<Dynamic> {
+        return untyped this[key];
+    }
+
+    @:arrayAccess inline function set(key:String, value: Dynamic): Dynamic {
+        return untyped this[key] = value;
+    }
+
+    inline function get__(): Dynamic<Dynamic> return this._;
+    inline function get_ctx(): Dynamic return this.ctx;
+    inline function get_linkCtx(): LinkCtx return this.linkCtx;
+    inline function get_linkedElem(): Null<Dynamic> return this.linkedElem;
+    inline function set_linkedElem(x): Null<Dynamic> return this.linkedElem = x;
+    inline function get_parentElem(): Element return this.parentElem;
+    inline function get_tagCtx(): TagCtx return this.tagCtx;
+    inline function get_tagName(): String return this.tagName;
+    inline function get_template(): Null<Template> return this.template;
+    inline function set_template(x): Null<Template> return this.template = x;
 }
 
+typedef TmplObject = {
+    var htmlTag(default, never): String;
+    var markup(default, never): String;
+    function render(?data: {}, ?helpers: JsObject<Dynamic -> Dynamic>, ?noIteration: Bool): String;
+}
+
+typedef LinkCtx = {
+    var attr(default, never): String;
+    var ctx(default, never): Dynamic;
+    var data(default, never): Dynamic;
+    var elem(default, never): Element;
+    var tag(default, never): TagDef;
+    var view(default, never): Dynamic;
+}
 
 typedef ConverterCtx = {
     var args(default, never): Array<Dynamic>;
@@ -155,30 +177,25 @@ typedef ConverterCtx = {
     var views(default, never): Dynamic;
 }
 
-private typedef Observable = {
+typedef Observable = {
     function observeAll(myHandler: ObservableEvent -> ObservableEventArgs -> Void): Void;
     function unobserveAll(myHandler: ObservableEvent -> ObservableEventArgs -> Void): Void;
-}
+    function data(): Dynamic;
 
-typedef ObjectObservable = {>Observable,
+    // for object
     @:overload(function (newValues: JsObject<Dynamic>): Template{})
-    function setProperty(path: String, value: Dynamic): ObjectObservable;
-    function data(): JsObject<Dynamic>;
-}
+    function setProperty(path: String, value: Dynamic): Observable;
 
-typedef ArrayObservable = {>Observable,
+    // for array
     @:overload(function (newItem: Dynamic): Template{})
-    function insert(index: Int, newItem: Dynamic): ArrayObservable;
+    function insert(index: Int, newItem: Dynamic): Observable;
 
-    function remove(index: Int, ?numToRemove: Int): ArrayObservable;
+    function remove(index: Int, ?numToRemove: Int): Observable;
 
-    function move(oldIndex: Int, newIndex: Int, ?numToMove: Int): ArrayObservable;
+    function move(oldIndex: Int, newIndex: Int, ?numToMove: Int): Observable;
 
-    function refresh(newItems: Array<Dynamic>): ArrayObservable;
-
-    function data(): Array<Dynamic>;
+    function refresh(newItems: Array<Dynamic>): Observable;
 }
-
 
 typedef ObservableEvent = {
     var target(default, never): Dynamic; // Object or Array<Dynamic>
@@ -252,11 +269,11 @@ abstract JsObject<A>(Dynamic<A>) from Dynamic<A> to Dynamic<A> {
         return cast this;
     }
 
-    @:arrayAccess public inline function arrayGet(key: String): Null<Dynamic> {
+    @:arrayAccess inline function get(key: String): Null<Dynamic> {
         return untyped this[key];
     }
 
-    @:arrayAccess public inline function arraySet(key:String, value: Dynamic): Dynamic {
+    @:arrayAccess inline function set(key:String, value: Dynamic): Dynamic {
         return untyped this[key] = value;
     }
 }
